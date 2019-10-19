f76482e390d0aed1693d1eb4960cd9da71ce3d78
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2019-10-19 19:28:35 +0200

    Clean apt repos

--- manifests/repo/apt.pp
+++ manifests/repo/apt.pp
@@ -5,8 +5,7 @@
 # @param release The release to use when building the repo URL
 # @param priority Apt pin priority to set for the Gluster repo
 #
-# Currently only released versions are supported.  If you want to use
-# QA releases or pre-releases, you'll need to edit line 54 below
+# @note Currently only released versions are supported.
 #
 # @example Enable the LATEST Apt repo for release 4.1
 #   class { gluster::repo::apt:
@@ -24,76 +23,39 @@
 # @note Copyright 2015 RL Solutions, unless otherwise noted
 #
 class gluster::repo::apt (
-  $version            = $gluster::version,
-  String[1] $release  = $gluster::release,
-  $priority           = $gluster::repo_priority,
-) {
-  include 'apt'
+  String $version = $gluster::params::version,
+  String $release = $gluster::params::release,
+  $priority = $gluster::params::repo_priority,
+) inherits gluster::params {
+  if $facts['os']['name'] != 'Debian' {
+    fail('gluster::repo::apt currently only works on Debian')
+  }
+
+  # the Gluster repo only supports x86_64 (amd64) and arm64.
+  unless $facts['architecture'] in ['amd64', 'arm64'] {
+    fail("Architecture ${facts['architecture']} not yet supported for ${facts['os']['name']}.")
+  }
 
   $repo_key_name = $release ? {
-    '3.10'       => 'C784DD0FD61E38B8B1F65E10DAD761554A72C1DF',
-    '3.11'       => 'DE82F0BACC4DB70DBEF95CA65EC2255642304A6E',
-    '3.12'       => '8B7C364430B66F0B084C0B0C55339A4C6A7BD8D4',
-    '3.13'       => '9B5AE8E6FD2581F293104ACC38675E5F30F779AF',
-    '4.0'        => '55F839E173AC06F364120D46FA86EEACB306CEE1',
-    '4.1'        => 'EED3351AFD72E5437C050F0388F6CDEE78FA6D97',
-    '^5\.(\d)+$' => 'F9C958A3AEE0D2184FAD1CBD43607F0DC2F8238C',
-    /^6/         => 'F9C958A3AEE0D2184FAD1CBD43607F0DC2F8238C',
-    /^7/         => 'F9C958A3AEE0D2184FAD1CBD43607F0DC2F8238C',
-    default      => '849512C2CA648EF425048F55C883F50CB2289A17',
+    '4.1'   => 'EED3351AFD72E5437C050F0388F6CDEE78FA6D97',
+    default => 'F9C958A3AEE0D2184FAD1CBD43607F0DC2F8238C',
   }
 
   $repo_key_source = "https://download.gluster.org/pub/gluster/glusterfs/${release}/rsa.pub"
+  $repo_url = "https://download.gluster.org/pub/gluster/glusterfs/${release}/${version}/Debian/${facts['lsbdistcodename']}/${facts['architecture']}/apt/"
 
-  # basic sanity check
-  if $version == 'LATEST' {
-    $repo_ver = $version
-  } elsif $version =~ /^\d\.\d+$/ {
-    $repo_ver = "${version}/LATEST"
-  } elsif $version =~ /^(\d)\.(\d+)\.(\d+).*$/ {
-    $repo_ver =  "${1}.${2}/${1}.${2}.${3}"
-  } else {
-    fail("${version} doesn't make sense for ${::operatingsystem}!")
-  }
-
-  # the Gluster repo only supports x86_64 (amd64) and arm64. The Ubuntu PPA also supports armhf and arm64.
-  case $facts['os']['name'] {
-    'Debian': {
-      $arch = $facts['architecture'] ? {
-        'amd64' => 'amd64',
-        'arm64' => 'arm64',
-        default => false,
-      }
-      if versioncmp($release, '3.12') < 0 {
-        $repo_url  = "https://download.gluster.org/pub/gluster/glusterfs/${release}/LATEST/Debian/${facts['lsbdistcodename']}/apt/"
-      } else {
-        $repo_url  = "https://download.gluster.org/pub/gluster/glusterfs/${release}/LATEST/Debian/${facts['lsbdistcodename']}/${arch}/apt/"
-      }
-    }
-    default: {
-      fail('gluster::repo::apt currently only works on Debian')
-    }
-  }
-  if ! $arch {
-    fail("Architecture ${facts['architecture']} not yet supported for ${facts['operatingsystem']}.")
-  }
-
-  $repo = {
-    "glusterfs-${version}" => {
-      ensure       => present,
-      location     => $repo_url,
-      repos        => 'main',
-      key          => {
-        id         => $repo_key_name,
-        key_source => $repo_key_source,
-      },
-      pin          => $priority,
-      architecture => $arch,
+  apt::source { "glusterfs-${release}-${version}":
+    ensure       => present,
+    location     => $repo_url,
+    repos        => 'main',
+    key          => {
+      id         => $repo_key_name,
+      key_source => $repo_key_source,
     },
+    pin          => $priority,
+    architecture => $facts['architecture'],
   }
 
-  create_resources(apt::source, $repo)
-
-  Apt::Source["glusterfs-${version}"] -> Package<| tag == 'gluster-packages' |>
+  Apt::Source["glusterfs-${release}-${version}"] -> Package<| tag == 'gluster-packages' |>
 
 }
--- spec/classes/repo_apt_spec.rb
+++ spec/classes/repo_apt_spec.rb
@@ -1,92 +1,63 @@
 require 'spec_helper'
 
-describe 'gluster::repo::apt', type: :class do
+describe 'gluster::repo::apt' do
   on_supported_os.each do |os, facts|
+    next unless facts[:osfamily] == 'Debian'
     context "on #{os}" do
-      let(:facts) do
-        facts
+      let(:facts) { facts }
+
+      context 'with all defaults' do
+        it { is_expected.to contain_class('gluster::params') }
+        it { is_expected.to contain_class('gluster::repo::apt') }
+        it { is_expected.to compile.with_all_deps }
+        it do
+          is_expected.to contain_apt__source('glusterfs-6-LATEST')
+            .with_key('id' => 'F9C958A3AEE0D2184FAD1CBD43607F0DC2F8238C', 'key_source' => 'https://download.gluster.org/pub/gluster/glusterfs/6/rsa.pub')
+            .with_location("https://download.gluster.org/pub/gluster/glusterfs/6/LATEST/Debian/#{facts[:lsbdistcodename]}/#{facts[:architecture]}/apt/")
+        end
       end
-      let :pre_condition do
-        'require ::gluster::params'
+
+      context 'unsupported architecture' do
+        let(:facts) { super().merge(architecture: 'zLinux') }
+        it { is_expected.to compile.and_raise_error(%r{not yet supported}) }
       end
 
-      case facts[:osfamily]
-      when 'Debian'
-        context 'with all defaults' do
-          it { is_expected.to contain_class('gluster::repo::apt') }
-          it { is_expected.to compile.with_all_deps }
-          it 'installs' do
-            is_expected.to contain_apt__source('glusterfs-LATEST').with(
-              repos: 'main',
-              location: "https://download.gluster.org/pub/gluster/glusterfs/3.12/LATEST/Debian/#{facts[:lsbdistcodename]}/#{facts[:architecture]}/apt/"
-            )
-          end
-        end
-        context 'unsupported architecture' do
-          let :facts do
-            super().merge(
-              architecture: 'zLinux'
-            )
-          end
-
-          it 'does not install' do
-            expect do
-              is_expected.to create_file('/etc/pki/rpm-gpg/RPM-GPG-KEY-gluster.pub')
-            end.to raise_error(Puppet::Error, %r{not yet supported})
-          end
-        end
-        context 'latest Gluster with priority' do
-          let :params do
-            {
-              priority: '700'
-            }
-          end
-
-          it 'installs' do
-            is_expected.to contain_apt__source('glusterfs-LATEST').with(
-              repos: 'main',
-              location: "https://download.gluster.org/pub/gluster/glusterfs/3.12/LATEST/Debian/#{facts[:lsbdistcodename]}/#{facts[:architecture]}/apt/",
-              pin: '700'
-            )
-          end
+      context 'latest Gluster with priority' do
+        let :params do
+          {
+            priority: '700'
+          }
         end
 
-        context 'Specific Gluster release 4.1' do
-          let :params do
-            {
-              release: '4.1'
-            }
-          end
+        it { is_expected.to contain_apt__source('glusterfs-6-LATEST').with_pin('700') }
+      end
 
-          it 'installs' do
-            is_expected.to contain_apt__source('glusterfs-LATEST').with(
-              repos: 'main',
-              key: {
-                'id' => 'EED3351AFD72E5437C050F0388F6CDEE78FA6D97',
-                'key_source' => 'https://download.gluster.org/pub/gluster/glusterfs/4.1/rsa.pub'
-              },
-              location: "https://download.gluster.org/pub/gluster/glusterfs/4.1/LATEST/Debian/#{facts[:lsbdistcodename]}/amd64/apt/"
-            )
-          end
+      context 'Specific Gluster release 4.1' do
+        let :params do
+          {
+            release: '4.1'
+          }
         end
 
-        context 'Specific Gluster release 3.12' do
-          let :params do
-            {
-              release: '3.12'
-            }
-          end
+        it do
+          is_expected.to contain_apt__source('glusterfs-4.1-LATEST')
+            .with_key('id' => 'EED3351AFD72E5437C050F0388F6CDEE78FA6D97', 'key_source' => 'https://download.gluster.org/pub/gluster/glusterfs/4.1/rsa.pub')
+            .with_location("https://download.gluster.org/pub/gluster/glusterfs/4.1/LATEST/Debian/#{facts[:lsbdistcodename]}/amd64/apt/")
+        end
+      end
 
-          it 'installs' do
-            is_expected.to contain_apt__source('glusterfs-LATEST').with(
-              repos: 'main',
-              key: {
-                'id' => '8B7C364430B66F0B084C0B0C55339A4C6A7BD8D4',
-                'key_source' => 'https://download.gluster.org/pub/gluster/glusterfs/3.12/rsa.pub'
-              },
-              location: "https://download.gluster.org/pub/gluster/glusterfs/3.12/LATEST/Debian/#{facts[:lsbdistcodename]}/amd64/apt/"
-            )
-          end
+      context 'Specific Gluster release 6.3' do
+        let :params do
+          {
+            release: '6',
+            version: '6.3'
+          }
+        end
+
+        it do
+          is_expected.to contain_apt__source('glusterfs-6-6.3')
+            .with_key('id' => 'F9C958A3AEE0D2184FAD1CBD43607F0DC2F8238C', 'key_source' => 'https://download.gluster.org/pub/gluster/glusterfs/6/rsa.pub')
+            .with_location("https://download.gluster.org/pub/gluster/glusterfs/6/6.3/Debian/#{facts[:lsbdistcodename]}/amd64/apt/")
         end
       end
     end
